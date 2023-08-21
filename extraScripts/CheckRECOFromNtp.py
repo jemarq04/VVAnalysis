93b2c973fcc0386b45482bb91fcc6ead8bcc86e9
hehe <he.he@cern.ch>
2023-08-21 18:39:04 +0200

    Update L1 script to have full mass range and UL xsecs

--- extraScripts/CheckRECOFromNtp.py
+++ extraScripts/CheckRECOFromNtp.py
@@ -48,12 +48,25 @@ def printr(l,ro):
 #VFP = "preVFP"
 legacy_datasets = ["ZZTo4L","GluGluToContinToZZTo4e","GluGluToContinToZZTo2e2mu","GluGluToContinToZZTo4mu"]
 pre_datasets = ["zz4l-powheg","ggZZ4e","ggZZ2e2mu","ggZZ4m"]
-xsecs = [1.256*1.0835,0.001586*1.7,0.003194*1.7,0.001586*1.7]
-fUL = ROOT.TFile("TreeFile_ZZSelector_Hists15Aug2023-ZZ4l2016_Moriondsel_Inclusive.root")
-fhUL = ROOT.TFile("Hists15Aug2023-ZZ4l2016_Moriond.root")
+xsecspre = [1.256*1.0835,0.001586*1.7,0.003194*1.7,0.001586*1.7]
+xsecsUL = [1.325 *1.0835,1.6175/1000.*1.7,3.29188/1000.*1.7,1.59718/1000.*1.7]
+checkgen = False
+if checkgen:
+    xsecsUL = xsecspre
+
+
+#on-shell ZZ files
+#fUL = ROOT.TFile("TreeFile_ZZSelector_Hists15Aug2023-ZZ4l2016_Moriondsel_Inclusive.root")
+#fhUL = ROOT.TFile("Hists15Aug2023-ZZ4l2016_Moriond.root")
+
+#Full mass range files
+fUL = ROOT.TFile("TreeFile_ZZSelector_Hists16Aug2023-ZZ4l2016_Moriondsel_Inclusive.root")
+fhUL = ROOT.TFile("Hists16Aug2023-ZZ4l2016_Moriond.root")
+
 #fpre = ROOT.TFile("TreeFile_ZZSelector_Hists25Jul2023-ZZ4l2016_Moriondsel_Inclusive.root")
 #fhpre = ROOT.TFile("CentralJet_Hists25Jul2023-ZZ4l2016_Moriond.root")
-varstr="nJets Mass mjj dEtajj jetPt[0] jetPt[1] absjetEta[0] absjetEta[1] Mass0j Mass1j Mass2j Mass3j Mass4j"
+#varstr="nJets Mass mjj dEtajj jetPt[0] jetPt[1] absjetEta[0] absjetEta[1] Mass0j Mass1j Mass2j Mass3j Mass4j"
+varstr="MassFull Mass0jFull Mass1jFull Mass2jFull Mass3jFull Mass4jFull"
 vars = varstr.split(" ")
 #vars = ["Mass","nJets","mjj"]
 #vars = ["nJets"]
@@ -65,8 +78,9 @@ lumi1 = 19.52
 lumi2 = 16.81
 hdict = {}
 initBins = {}
-#weightExpr = "genWeight"
 weightExpr = "weight"
+if checkgen:
+    weightExpr = "genWeight"
 
 #=========================================
 initBins["nJets"] = "8,0,8"
@@ -76,6 +90,14 @@ initBins["Mass1j"] = "1215,70,2500"
 initBins["Mass2j"] = "1215,70,2500"
 initBins["Mass3j"] = "1215,70,2500"
 initBins["Mass4j"] = "1215,70,2500"
+
+initBins["MassFull"] = "1215,70,2500"
+initBins["Mass0jFull"] = "1215,70,2500"
+initBins["Mass1jFull"] = "1215,70,2500"
+initBins["Mass2jFull"] = "1215,70,2500"
+initBins["Mass3jFull"] = "1215,70,2500"
+initBins["Mass4jFull"] = "1215,70,2500"
+
 initBins["mjj"] = "60,0,1200"
 initBins["dEtajj"] = "80,0,8"
 initBins["jetPt[0]"] = "50,30,530"
@@ -89,7 +111,7 @@ for var in vars:
     #For each channel form 3 histograms
     for chan in channels:
         hdict[var][chan] = []
-        print("Running channel: %s"%chan)
+        #print("Running channel: %s"%chan)
         histname = var+"_"+chan
 
         #Have to do this manually for now
@@ -106,6 +128,7 @@ for var in vars:
                 datasets = pre_datasets
                 fin = fUL
                 fh = fhUL
+                xsecs = xsecspre
                 #tag = ""
                 additional = ""
                 lumipairs = [(lumitot,"")]
@@ -113,6 +136,7 @@ for var in vars:
                 datasets = legacy_datasets
                 fin = fUL
                 fh = fhUL
+                xsecs = xsecsUL
                 #tag = VFP + ("L1Full" if i==1 else "L1ECAL")
                 additional = ""
                 lumipairs = [(lumi1,"preVFP"),(lumi2,"postVFP")]
@@ -139,7 +163,7 @@ for var in vars:
                         if "abs" in varp:
                             exec("h.Fill(abs(evt.%s),evt.%s*xsecs[j]*lumi*1000/sumweights%s)"%(varp.replace("abs",""),weightExpr,additional))
                         elif "Mass" in var and "j" in var:
-                            tmp_nj = int(var.replace("Mass","").replace("j",""))
+                            tmp_nj = int(var.replace("Mass","").replace("j","").replace("Full","") )
                             if tmp_nj == 4:
                                 if evt.nJets>=4:    
                                     exec("h.Fill(evt.Mass,evt.%s*xsecs[j]*lumi*1000/sumweights%s)"%(weightExpr,additional))
@@ -147,6 +171,7 @@ for var in vars:
                                 if evt.nJets == tmp_nj:
                                     exec("h.Fill(evt.Mass,evt.%s*xsecs[j]*lumi*1000/sumweights%s)"%(weightExpr,additional))
                         else:
+                            varp = varp.replace("Full","") #Full and on-shell variables aren't processed together
                             exec("h.Fill(evt.%s,evt.%s*xsecs[j]*lumi*1000/sumweights%s)"%(varp,weightExpr,additional))
             
             h = rebin(h,var)
@@ -171,11 +196,18 @@ for var in vars:
         if chan == "mmee":
             continue
         print(chan)
-        print(percentDiff(hdict[var][chan][1],hdict[var][chan][2]))
+        perlist = percentDiff(hdict[var][chan][1],hdict[var][chan][2])
+        print(perlist)
+        for perc in perlist:
+            if chan == "total" and perc>=3.:
+                print("WARNING: >3 percent for one entry in total")
+            
+            if chan == "mmmm" and perc>=5.:
+                print("WARNING: >5 percent for one entry in 4m")
         
-        print("Weighted events prelegacy and legacy for ECAL weight")
-        print(hdict[var][chan][0].Integral(1,hdict[var][chan][0].GetNbinsX()))
-        print(hdict[var][chan][2].Integral(1,hdict[var][chan][0].GetNbinsX()))
+        #print("Weighted events prelegacy and legacy for ECAL weight")
+        #print(hdict[var][chan][0].Integral(1,hdict[var][chan][0].GetNbinsX()))
+        #print(hdict[var][chan][2].Integral(1,hdict[var][chan][0].GetNbinsX()))
     
 fout = ROOT.TFile("L1HistOutput.root","RECREATE")
 fout.cd()
@@ -186,3 +218,4 @@ for var in vars:
         for h in hdict[var][chan]:
             h.Write()
 fout.Close()
+print("All variables processed")
