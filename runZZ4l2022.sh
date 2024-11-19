7716edc7850ea94f7ad1c9f69c72ea7413c4b732
Justin Marquez <justin.marquez@wisc.edu>
2024-11-19 15:39:12 -0600

    add 2022 merging script

---
+++ runZZ4l2022.sh
@@ -0,0 +1,5 @@
+#NOTE: Eventually the scale factor file listed below will exist with fake rates.
+#   For now, use this in the call so that the appropriate scale factors are retrieved with correctionlib
+./Utilities/scripts/makeHistFile.py -f ZZ4l2022 -a ZZ4l2022 -s LooseLeptons --output_file test2022 --muIDType MVA --year 2022 --uwvv -c eemm -j 12 --with_background -sf data/latestSFs/scaleFactorsZZ4l2022.root #--with_Gen
+
+echo "Job done!!==================================="
