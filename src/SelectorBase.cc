ca3a6d0deb5ff05d445d5054ddaa99dac6329c97
Justin Marquez <justin.marquez@wisc.edu>
2025-01-24 13:39:44 -0600

    remove unnecessary warning when merging

--- src/SelectorBase.cc
+++ src/SelectorBase.cc
@@ -262,8 +262,9 @@ void SelectorBase::InitializeHistogramsFromConfig()
                 InitializeHistogramFromConfig(name, chan, histData);
             }
             // No need to print warning for every channel
-            else if (chan == channels.front())
-                std::cerr << "Skipping invalid histogram " << name << std::endl;
+            // No need to print it at all...
+            //else if (chan == channels.front())
+            //    std::cerr << "Skipping invalid histogram " << name << std::endl;
         }
     }
 }
