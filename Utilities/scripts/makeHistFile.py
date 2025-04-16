04388e903c1def89ed84a07bd6ebfd1d47e8c297
Justin Marquez <justin.marquez@wisc.edu>
2025-04-16 09:11:09 -0500

    clean up optional inputs

--- Utilities/scripts/makeHistFile.py
+++ Utilities/scripts/makeHistFile.py
@@ -91,10 +91,8 @@ def makeHistFile(args):
             sf_inputs = [basename, yearcfg] 
             
             # Optional inputs
-            qqZZ_kfac = ROOT.TNamed("qqZZ_kfac", "data/qqZZ_kfacs.json")
-            #sf_inputs.append(qqZZ_kfac)
-            eIdSFs = ROOT.TNamed("eIdSf", "data/ElectronSF_HZZUL.json")
-            #sf_inputs.append(eIdSFs)
+            #sf_inputs.append(ROOT.TNamed("qqZZ_kfac", "data/qqZZ_kfacs.json"))
+            #sf_inputs.append(ROOT.TNamed("eIdSf", "data/ElectronSF_HZZUL.json"))
         else:
             # The lines below use Run 2
             fScales = ROOT.TFile('data/scaleFactors.root')
