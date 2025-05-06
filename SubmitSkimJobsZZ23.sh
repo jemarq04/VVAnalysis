d2580ccd8c325068c085ae9c3e70781c1fc794d0
Justin Marquez <justin.marquez@wisc.edu>
2025-05-06 14:59:17 -0500

    add 2023 file

---
+++ SubmitSkimJobsZZ23.sh
@@ -0,0 +1,3 @@
+#./farmoutNtupleSkim.py -a ZZ4l2023 -s loosePreselection,Zselection,Overlap,QCDVeto,4lmass -f ZZ4l2023 -e-gen 
+
+./farmoutNtupleSkim.py -a ZZ4l2023 -s loosePreselection,Zselection,Overlap,QCDVeto,4lmass -f ZZ4l2023
