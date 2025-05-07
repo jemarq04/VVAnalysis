ba687d30be261f1b0ebdeaf11319a201ebaa0fe9
Justin Marquez <justin.marquez@wisc.edu>
2025-05-07 10:22:04 -0500

    create helper script that allows for quick job submissions. created a different bash script to submit custom commands

--- SubmitSkimJobsZZ23.sh
+++
@@ -1,3 +0,0 @@
-#./farmoutNtupleSkim.py -a ZZ4l2023 -s loosePreselection,Zselection,Overlap,QCDVeto,4lmass -f ZZ4l2023 -e-gen 
-
-./farmoutNtupleSkim.py -a ZZ4l2023 -s loosePreselection,Zselection,Overlap,QCDVeto,4lmass -f ZZ4l2023
