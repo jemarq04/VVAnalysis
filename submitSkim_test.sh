22008ea32343f8378c9ddfed38ca2689900ffb1d
Justin Marquez <justin.marquez@wisc.edu>
2025-05-30 15:16:15 -0500

    update file

--- submitSkim_test.sh
+++ submitSkim_test.sh
@@ -1,5 +1,7 @@
-./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection,Zselection,Overlap,QCDVeto,4lmass -f ZZ4l2022
+./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection -f ZZ4l2022
+#./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection,Zselection,Overlap,QCDVeto,4lmass -f ZZ4l2022
 #./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection,Zselection,Overlap,QCDVeto,4lmass,final -f ZZ4l2022
+
 #./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection,Zselection,Overlap,QCDVeto,4lmass,finalLLR -f ZZ4l2022
 #./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection,Zselection,Overlap,QCDVeto,4lmass,finalLLRAlt -f ZZ4l2022
 #./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection,Zselection,Overlap,QCDVeto,4lmass,tightPtCut -f ZZ4l2022
