592a528e1729ae13cacd0b4769aa704013700080
Justin Marquez <justin.marquez@wisc.edu>
2025-06-20 16:49:01 -0500

    turn on doSystematics_

--- src/ZZSelector.cc
+++ src/ZZSelector.cc
@@ -15,7 +15,7 @@ void ZZSelector::Init(TTree *tree)
     {pileupUp, "CMS_pileupUp"},
     {pileupDown, "CMS_pileupDown"},
   };
-  doSystematics_ = false;
+  doSystematics_ = true;
 
   // This would be set true inside ZZBackground Selector
   // isNonPrompt_ = false;
