ea35b34a0afdbe9a452b6c029419aa58c3ab1e03
Justin Marquez <justin.marquez@wisc.edu>
2025-08-15 14:00:42 -0500

    update test script for use with run 3 sync check

--- submitSkim_test.sh
+++ submitSkim_test.sh
@@ -1,7 +1,2 @@
-./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection -f ZZ4l2022
-#./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection,Zselection,Overlap,QCDVeto,4lmass -f ZZ4l2022
-#./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection,Zselection,Overlap,QCDVeto,4lmass,final -f ZZ4l2022
-
-#./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection,Zselection,Overlap,QCDVeto,4lmass,finalLLR -f ZZ4l2022
-#./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection,Zselection,Overlap,QCDVeto,4lmass,finalLLRAlt -f ZZ4l2022
-#./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection,Zselection,Overlap,QCDVeto,4lmass,tightPtCut -f ZZ4l2022
+./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection -f ZZ4l2022 --jobName ZZ4l2022AnalysisJobs_step1
+#./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection,Zselection,Overlap,QCDVeto,tightIsoLeps,4lmass,final,finalLLRAlt -f ZZ4l2022 --jobName ZZ4l2022AnalysisJobs_finalLLR
