4dc6a24e6f35be5b8dedc1117ad34ea858dab127
Justin Marquez <justin.marquez@wisc.edu>
2025-08-18 14:47:48 -0500

    update scripts

--- makeFakeRates2022.sh
+++ makeFakeRates2022.sh
@@ -1,1 +1,1 @@
-./Utilities/scripts/makeFakeRates.py -a ZZ4l2022 -s ZplusLSkim -l 34.652 -f ZZ4l2022 --uwvv --noHistConfig -c eee,eem,emm,mmm --test
+./Utilities/scripts/makeFakeRates.py -a ZZ4l2022 -f ZZ4l2022 -y 2022 -s ZplusLSkim -l 34.652 --uwvv --noHistConfig -c eee,eem,emm,mmm --test
