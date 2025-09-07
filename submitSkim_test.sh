da2a64e0058f3589d0d99ebbff6f83d84da1a99d
Justin Marquez <justin.marquez@wisc.edu>
2025-09-07 17:24:32 -0500

    remove tightIsoLeps from cutflow - needed for nonprompt background estimation

--- submitSkim_test.sh
+++ submitSkim_test.sh
@@ -1,2 +1,2 @@
 ./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection -f ZZ4l2022 --jobName ZZ4l2022AnalysisJobs_step1
-#./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection,Zselection,Overlap,QCDVeto,tightIsoLeps,4lmass,final,finalLLRAlt -f ZZ4l2022 --jobName ZZ4l2022AnalysisJobs_finalLLR
+#./farmoutNtupleSkim.py -a ZZ4l2022 -s loosePreselection,Zselection,Overlap,QCDVeto,4lmass,final,finalLLRAlt -f ZZ4l2022 --jobName ZZ4l2022AnalysisJobs_finalLLR
