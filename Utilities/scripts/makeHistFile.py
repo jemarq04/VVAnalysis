302825b47790912abdc6614899a195018bdba41d
Justin Marquez <justin.marquez@wisc.edu>
2025-09-12 08:23:57 -0500

    fix typo

--- Utilities/scripts/makeHistFile.py
+++ Utilities/scripts/makeHistFile.py
@@ -86,7 +86,7 @@ def makeHistFile(args):
                 fr_inputs = [eZZTightFakeRate, mZZTightFakeRate]
 
             #basename = ROOT.TNamed("basename", "/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG")
-            basename = ROOT.TNamed("basename", "%s/src/Analysis/VVAnalysis/data/POG", os.environ["CMSSW_BASE"])
+            basename = ROOT.TNamed("basename", "%s/src/Analysis/VVAnalysis/data/POG" % os.environ["CMSSW_BASE"])
             yearcfg = ROOT.TNamed("yearcfg", args["year"])
 
             sf_inputs = [basename, yearcfg] 
