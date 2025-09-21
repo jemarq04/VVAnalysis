00418fe4b577fc1f006586b98c96842146126157
Justin Marquez <justin.marquez@wisc.edu>
2025-09-20 19:02:03 -0500

    add fake rates to merged file

--- runZZ4l.sh
+++ runZZ4l.sh
@@ -15,11 +15,12 @@ for yr in $year; do
   if [[ $yr = 2022 ]]; then lumi=34.652;
   elif [[ $yr = 2023 ]]; then lumi=27.76;
   fi
+  frfile=data/fakeScaleFactorsRun3-ZZ4l$yr.root
 
   #NOTE: Eventually the scale factor file listed below will exist with fake rates.
   #   For now, use this in the call so that the appropriate scale factors are retrieved with correctionlib
-  ./Utilities/scripts/makeHistFile.py -f ZZ4l$yr -l $lumi -a ZZ4l$yr -s LooseLeptons --output_file "test$yr" --year $yr --uwvv -c eeee,eemm,mmee,mmmm -j 12 -sf #--with_Gen
-  #./Utilities/scripts/makeHistFile.py -f ZZ4l$yr -l $lumi -a ZZ4l$yr -s LooseLeptons --output_file "test$yr" --year $yr --uwvv -c eeee,eemm,mmee,mmmm -j 12 -sf --with_background -F HistFiles/fakeRate-ZZ4l$yr.root
+  #./Utilities/scripts/makeHistFile.py -f ZZ4l$yr -l $lumi -a ZZ4l$yr -s LooseLeptons --output_file "test$yr" --year $yr --uwvv -c eeee,eemm,mmee,mmmm -j 12 -sf #--with_Gen
+  ./Utilities/scripts/makeHistFile.py -f ZZ4l$yr -l $lumi -a ZZ4l$yr -s LooseLeptons --output_file "test$yr" --year $yr --uwvv -c eeee,eemm,mmee,mmmm -j 12 -sf --with_background -F $frfile
 
   echo "$yr done!!==================================="
 done
