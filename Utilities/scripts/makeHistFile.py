5d04a6c50f28b36959ac7c0a74eeabdb2532ab34
Justin Marquez <justin.marquez@wisc.edu>
2025-10-25 19:15:25 -0500

    fix typo to properly pass lepton ID SFs

--- Utilities/scripts/makeHistFile.py
+++ Utilities/scripts/makeHistFile.py
@@ -93,8 +93,8 @@ def makeHistFile(args):
             
             # Optional inputs
             #sf_inputs.append(ROOT.TNamed("qqZZ_kfac", "data/qqZZ_kfacs.json"))
-            sf_inputs.append(ROOT.TNamed("eIdSf", "data/ElectronSF_HZZ.json"))
-            sf_inputs.append(ROOT.TNamed("mIdSf", "data/MuonSF_HZZ.json"))
+            sf_inputs.append(ROOT.TNamed("eIdSF", "data/ElectronSF_HZZ.json"))
+            sf_inputs.append(ROOT.TNamed("mIdSF", "data/MuonSF_HZZ.json"))
         else:
             # The lines below use Run 2
             fScales = ROOT.TFile('data/scaleFactors.root')
