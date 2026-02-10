3f460abbe3256c92345a3aff8d2c781fe4a910d5
Justin Marquez <justin.marquez@wisc.edu>
2026-02-10 14:14:03 -0600

    fix: remove outdated --uwvv flag

--- runZZ4l.sh
+++ runZZ4l.sh
@@ -19,9 +19,9 @@ for yr in $year; do
   [[ ! -z $2 ]] && frfile=$2
 
   # Without nonprompt contribution
-  #./Utilities/scripts/makeHistFile.py -f ZZ4l$yr -a ZZ4l$yr -s LooseLeptons --year $yr --uwvv -c eeee,eemm,mmee,mmmm -j 12 -sf #--with_Gen
+  #./Utilities/scripts/makeHistFile.py -f ZZ4l$yr -a ZZ4l$yr -s LooseLeptons --year $yr -c eeee,eemm,mmee,mmmm -j 12 -sf #--with_Gen
 
-  ./Utilities/scripts/makeHistFile.py -f ZZ4l$yr -a ZZ4l$yr -s LooseLeptons --year $yr --uwvv -c eeee,eemm,mmee,mmmm -j 12 -sf --with_background -F $frfile
+  ./Utilities/scripts/makeHistFile.py -f ZZ4l$yr -a ZZ4l$yr -s LooseLeptons --year $yr -c eeee,eemm,mmee,mmmm -j 12 -sf --with_background -F $frfile
 
   echo "$yr done!!==================================="
 done
