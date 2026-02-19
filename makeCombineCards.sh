0efff1297563d196e670e23f8763d62f8d507e44
Justin Marquez <justin.marquez@wisc.edu>
2026-02-19 14:26:46 -0600

    feat: add helper script to create combine cards

---
+++ makeCombineCards.sh
@@ -0,0 +1,1 @@
+python3 Utilities/scripts/setupZZCombineRun3.py -i HistFiles/Hists12Feb2026-ZZ4l2022.root -o temp3 --rebin 100,200,250,300,350,400,500,600,800,1000 -c eeee,eemm,mmee,mmmm -a ZZ4l2022 2022
