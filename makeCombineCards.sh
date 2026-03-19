25a64c07bc3e5f2108baaa44be10ae65a676679c
Justin Marquez <justin.marquez@wisc.edu>
2026-03-19 14:52:54 -0500

    fix: add shebang

--- makeCombineCards.sh
+++ makeCombineCards.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo 2022
 python3 Utilities/scripts/setupZZCombineRun3.py -i HistFiles/Hists26Feb2026-ZZ4l2022.root -o CombineCards --rebin 100,200,250,300,350,400,500,600,800,1000 -c eeee,eemm,mmee,mmmm -a ZZ4l2022 2022
 
