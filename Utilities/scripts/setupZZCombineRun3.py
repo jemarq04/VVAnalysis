50fa2ad244525acc0e615f7d366af96925ffb55a
Justin Marquez <justin.marquez@wisc.edu>
2026-04-22 13:27:55 -0500

    chore: remove comment

--- Utilities/scripts/setupZZCombineRun3.py
+++ Utilities/scripts/setupZZCombineRun3.py
@@ -39,7 +39,7 @@ def main():
     lumi_info = ConfigureJobs.getLumiMap()
     years = lumi_info["Run3Combined"]["years"]
     lumiMap = {int(year): float("%.3f" % ConfigureJobs.getLuminosity(year)) for year in years}
-    lumiUncMap = {  # TODO: 2024 still set to 0.0
+    lumiUncMap = {
         int(year): lumi_info[year]["unc"] for year in years
     }
     sig_procs = ["qqZZ-powheg"]
