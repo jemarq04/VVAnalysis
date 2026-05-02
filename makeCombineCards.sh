0af54aca0899769aad5c42cb0d5de41653d06506
Justin Marquez <justin.marquez@wisc.edu>
2026-05-02 09:36:24 -0500

    feat: change default input file name

--- makeCombineCards.sh
+++ makeCombineCards.sh
@@ -6,7 +6,7 @@ years="2022 2023 2024"
 for year in $years; do
   echo Making datacard for $year
 
-  infile="HistFiles/Hists26Feb2026-ZZ4l$year.root"
+  infile="HistFiles/SystHists-ZZ4l$year.root"
 
   python3 Utilities/scripts/setupZZCombineRun3.py -i $infile -o $outdir --rebin 100,200,250,300,350,400,500,600,800,1000 -c eeee,eemm,mmee,mmmm -a ZZ4l$year $year
 done
