d74cfd8cf7f66928771bcd6bcb4b0cf1638a787a
Justin Marquez <justin.marquez@wisc.edu>
2026-05-06 11:25:31 -0500

    fix: allow new eras to be copied when found

--- data/check_corrections.sh
+++ data/check_corrections.sh
@@ -6,6 +6,7 @@ eras=(
   Run3-23CSep23-Summer23-NanoAODv12
   Run3-23DSep23-Summer23BPix-NanoAODv12
   Run3-24CDEReprocessingFGHIPrompt-Summer24-NanoAODv15
+  Run3-25Prompt-Summer24-NanoAODv15
 )
 
 POGs=(
@@ -16,15 +17,13 @@ POGs=(
 basepath="/cvmfs/cms-griddata.cern.ch/cat/metadata"
 outdir="XPOG"
 for pog in "${POGs[@]}"; do
-  if [[ ! -d $outdir/$pog ]]; then #first time running, copy files over
-    for era in "${eras[@]}"; do
+  for era in "${eras[@]}"; do
+    if [[ ! -d $outdir/$pog/$era ]]; then
+      [[ ! -d $basepath/$pog/$era ]] && continue
       mkdir -p $outdir/$pog/$era
       cp -vr $basepath/$pog/$era/latest $outdir/$pog/$era
-    done
-    continue
-  fi
-
-  for era in "${eras[@]}"; do
+      continue
+    fi
     for f in $outdir/$pog/$era/latest/*.json.gz; do
       [[ ! -f $f ]] && continue
       centralfile="$basepath/$pog/$era/latest/$(basename $f)"
@@ -33,12 +32,11 @@ for pog in "${POGs[@]}"; do
         continue
       fi
       if ! diff $f $centralfile >& /dev/null; then
-        echo 
-        echo $f is out-of-date.
+        echo
+        echo ${f/$outdir\//} is out-of-date.
         read -p "Update the file? (y/n): " choice
         [[ $choice == [yY] ]] && cp -v $centralfile $f
       fi
     done
   done
-
 done
